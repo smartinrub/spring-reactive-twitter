f2ba3d18a97d5a39fccd20736a751db37d78c95c
Sergio Martin Rubio <smartinrubio@hotels.com>
2019-05-14 11:39:30 +0100

    config twitter4j client

--- src/main/java/com/sergiomartinrubio/springreactivetwitter/DataInitializer.java
+++ src/main/java/com/sergiomartinrubio/springreactivetwitter/DataInitializer.java
@@ -3,20 +3,18 @@ package com.sergiomartinrubio.springreactivetwitter;
 import lombok.AllArgsConstructor;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
-import org.springframework.social.twitter.api.Twitter;
 import org.springframework.stereotype.Component;
-
-import java.util.List;
+import twitter4j.TwitterStream;
 
 @Component
 @AllArgsConstructor
 public class DataInitializer implements ApplicationRunner {
 
-    private final Twitter twitter;
-    private final TweetsStreamListener listener;
+    private final TwitterStream twitterStream;
+    private final StreamListener listener;
 
     @Override
     public void run(ApplicationArguments args) {
-        twitter.streamingOperations().filter(args.getNonOptionArgs().get(0), List.of(listener));
+        twitterStream.addListener(listener).filter(args.getNonOptionArgs().get(0));
     }
 }
